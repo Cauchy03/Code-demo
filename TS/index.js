0ca220b6b0285cdfc28d303ec420273a3c6c36cc
Cauchy03 <1481856337@qq.com>
2023-08-05 23:16:13 +0800

    :see_no_evil: update .gitignore

--- TS/index.js
+++
@@ -1,4 +0,0 @@
-// console.log(Tristate[0]); // 'False'
-console.log(0 /* Tristate['False'] */); // 0
-var x = 2 /* Tristate.Unknown */;
-// console.log(Tristate[Tristate.False]); // 'False' because `Tristate.False == 0`
